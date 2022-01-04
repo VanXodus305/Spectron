57536da9974fedd084ffc9a7dd18481956343d87
VanXodus305 <soumyadeep.30505@gmail.com>
2022-01-04 23:11:54 +0530

    Making Slash Commands

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function (o, m, k, k2) {
     if (k2 === undefined) k2 = k;
-    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
-}) : (function(o, m, k, k2) {
+    Object.defineProperty(o, k2, { enumerable: true, get: function () { return m[k]; } });
+}) : (function (o, m, k, k2) {
     if (k2 === undefined) k2 = k;
     o[k2] = m[k];
 }));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function (o, v) {
     Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
+}) : function (o, v) {
     o["default"] = v;
 });
 var __importStar = (this && this.__importStar) || function (mod) {
@@ -18,13 +18,13 @@ var __importStar = (this && this.__importStar) || function (mod) {
     __setModuleDefault(result, mod);
     return result;
 };
+var _a;
 Object.defineProperty(exports, "__esModule", { value: true });
 const dotenv_1 = require("dotenv");
 (0, dotenv_1.config)();
 const discord_js_1 = __importStar(require("discord.js"));
 const intents = [discord_js_1.Intents.FLAGS.GUILDS, discord_js_1.Intents.FLAGS.GUILD_BANS, discord_js_1.Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS, discord_js_1.Intents.FLAGS.GUILD_INTEGRATIONS, discord_js_1.Intents.FLAGS.GUILD_INVITES, discord_js_1.Intents.FLAGS.GUILD_MEMBERS, discord_js_1.Intents.FLAGS.GUILD_MESSAGES, discord_js_1.Intents.FLAGS.GUILD_MESSAGE_REACTIONS, discord_js_1.Intents.FLAGS.GUILD_MESSAGE_TYPING, discord_js_1.Intents.FLAGS.GUILD_PRESENCES, discord_js_1.Intents.FLAGS.GUILD_VOICE_STATES, discord_js_1.Intents.FLAGS.GUILD_WEBHOOKS, discord_js_1.Intents.FLAGS.DIRECT_MESSAGES, discord_js_1.Intents.FLAGS.DIRECT_MESSAGE_REACTIONS, discord_js_1.Intents.FLAGS.DIRECT_MESSAGE_TYPING];
 const client = new discord_js_1.default.Client({ intents: intents });
-client.login(process.env.DISCORD_TOKEN);
 client.once('ready', () => {
     console.log(`${client.user.tag} has logged in successfully.`);
 });
@@ -33,3 +33,31 @@ client.on('messageCreate', (msg) => {
         msg.reply('**Pong!**');
     }
 });
+const guildId = '746313837049020517'; // Use your guild ID instead
+const guild = client.guilds.cache.get(guildId);
+let commands;
+if (guild) {
+    commands = guild.commands;
+}
+else {
+    commands = (_a = client.application) === null || _a === void 0 ? void 0 : _a.commands;
+}
+commands === null || commands === void 0 ? void 0 : commands.create({
+    name: 'add',
+    description: 'Adds two numbers',
+    options: [
+        {
+            name: 'number1',
+            description: 'The first number.',
+            required: true,
+            type: discord_js_1.default.Constants.ApplicationCommandOptionTypes.NUMBER,
+        },
+        {
+            name: 'number2',
+            description: 'The second number.',
+            required: true,
+            type: discord_js_1.default.Constants.ApplicationCommandOptionTypes.NUMBER,
+        },
+    ],
+});
+client.login(process.env.DISCORD_TOKEN);
--- index.ts
+++ index.ts
@@ -1,10 +1,8 @@
-import {config} from 'dotenv';
+import { config } from 'dotenv';
 config();
 import Discord, { Intents, MessageEmbed } from 'discord.js';
 const intents = [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_BANS, Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS, Intents.FLAGS.GUILD_INTEGRATIONS, Intents.FLAGS.GUILD_INVITES, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Intents.FLAGS.GUILD_MESSAGE_TYPING, Intents.FLAGS.GUILD_PRESENCES, Intents.FLAGS.GUILD_VOICE_STATES, Intents.FLAGS.GUILD_WEBHOOKS, Intents.FLAGS.DIRECT_MESSAGES, Intents.FLAGS.DIRECT_MESSAGE_REACTIONS, Intents.FLAGS.DIRECT_MESSAGE_TYPING];
 const client = new Discord.Client({ intents: intents });
-
-client.login(process.env.DISCORD_TOKEN);
 
 client.once('ready', () => {
   console.log(`${client.user!.tag} has logged in successfully.`);
@@ -15,3 +13,32 @@ client.on('messageCreate', (msg) => {
     msg.reply('**Pong!**');
   }
 });
+
+const guildId = '746313837049020517' // Use your guild ID instead
+const guild = client.guilds.cache.get(guildId)
+let commands
+if (guild) {
+  commands = guild.commands
+} else {
+  commands = client.application?.commands
+}
+commands?.create({
+  name: 'add',
+  description: 'Adds two numbers',
+  options: [
+    {
+      name: 'number1',
+      description: 'The first number.',
+      required: true,
+      type: Discord.Constants.ApplicationCommandOptionTypes.NUMBER,
+    },
+    {
+      name: 'number2',
+      description: 'The second number.',
+      required: true,
+      type: Discord.Constants.ApplicationCommandOptionTypes.NUMBER,
+    },
+  ],
+})
+
+client.login(process.env.DISCORD_TOKEN);
