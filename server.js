cb31c3455dc448edc11dcf8e15ddd189647c4145
VanXodus305 <soumyadeep.30505@gmail.com>
2022-09-11 21:02:29 +0530

    Updated server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express")
 const server = express()
 
 server.all("/", (req, res) => {
-  res.send("Bot is running!")
+  res.send("Spectron#6051 is online.")
 })
 
 function keepAlive() {
